2d91c2a1ed48936db369d8297c32850b5e29d862
Myiah Moore <myiahsade@gmail.com>
2018-07-31 22:28:48 -0500

    check in

---
+++ EX22.js
@@ -0,0 +1,11 @@
+main();
+function main()
+{
+    var userInput = prompt("Enter a word or press 'q' to quit: ");
+        while (userInput != "q")
+        {
+            userInput = prompt("try again ")
+        }
+
+
+}
---
+++ EX23.js
@@ -0,0 +1,10 @@
+main();
+ function main()
+ {
+ var password = "C0d3Cr3w"
+    userInput = prompt("Enter password: ");
+     while (password!= userInput)
+     {
+     userInput = prompt("Try again")
+     }
+ }
