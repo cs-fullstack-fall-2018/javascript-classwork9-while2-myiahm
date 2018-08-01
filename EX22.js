691a952792484bfb97992d74407888e138fa8f45
Kevin-CodeCrew <kevin@codecrew.org>
2018-08-01 07:18:36 -0500

    Graded 08-01-2018 07:16:39

--- EX22.js
+++ EX22.js
@@ -2,10 +2,17 @@ main();
 function main()
 {
     var userInput = prompt("Enter a word or press 'q' to quit: ");
-        while (userInput != "q")
+        while (userInput !== "q")
         {
             userInput = prompt("try again ")
         }
 
-
 }
+
+/*
+
+	KEY: This looks good, but where are your comments?!
+
+	Also, get in the habit of using '===' to test for equality and '!==' to test for inequality.
+
+*/
--- EX23.js
+++ EX23.js
@@ -7,4 +7,15 @@ main();
      {
      userInput = prompt("Try again")
      }
+
+     // Missing the ```THAT’S CORRECT!!!``` success alert
+     alert("THAT'S CORRECT!!!");
  }
+
+ /*
+
+	KEY: This looks good for the most part, but where are your comments?!
+
+	Also, get in the habit of using '===' to test for equality and '!==' for inequality. 
+
+*/
